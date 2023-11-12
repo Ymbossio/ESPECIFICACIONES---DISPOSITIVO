ad16f18502dba9402b95c273a988257be045aedb
Yovanis Bossio <yovanis2001@gmail.com>
2023-11-12 02:33:44 -0500

    Primeros pasos en NODE JS

---
+++ index.js
@@ -0,0 +1,12 @@
+const os = require('node:os');
+
+
+console.log('Información del sistema operativo');
+console.log('-------------------------------------------------------------------------------');
+
+console.log('Nombre del sistema operativo: ', os.platform()); 
+console.log('Versión del sistema operativo: ', os.release());
+console.log('Arquitectura: ', os.arch());
+console.log('CPUS: ', os.cpus());
+console.log('Memoria libre: ', os.freemem());
+console.log('Memoria Total: ', os.totalmem());
