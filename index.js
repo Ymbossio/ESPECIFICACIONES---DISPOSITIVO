d59560b8880814c8db6948c877a71e3aee0deaf9
Yovanis Bossio <yovanis2001@gmail.com>
2023-11-12 02:36:29 -0500

    mini cambios

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const os = require('node:os');
 
 
 console.log('Información del sistema operativo');
-console.log('-------------------------------------------------------------------------------');
+console.log('--------------------------------------------------------------------------------------------------------------------------------------');
 
 console.log('Nombre del sistema operativo: ', os.platform()); 
 console.log('Versión del sistema operativo: ', os.release());
@@ -10,3 +10,5 @@ console.log('Arquitectura: ', os.arch());
 console.log('CPUS: ', os.cpus());
 console.log('Memoria libre: ', os.freemem());
 console.log('Memoria Total: ', os.totalmem());
+
+console.log('--------------------------------------------------------------------------------------------------------------------------------------');
